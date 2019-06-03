1b7929a8e0618a73defee772f725ef6bd269814b
bharath <bharathjinka09@gmail.com>
2019-06-03 10:52:08 +0530

    Added login form files

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("register", views.register, name="register"),]
+urlpatterns = [
+    path("register", views.register, name="register"),
+    path("login", views.login, name="login"),
+
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.contrib.auth.models import User,auth
+from django.contrib.auth import authenticate,login 
 
 # Create your views here.
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+        else:
+            messages.error(request,'Invalid Credentials')
+            return redirect('login')
+
+        
+    else:
+        return render(request,'login.html')
+
+
 def register(request):
 
     if request.method == 'POST':
@@ -26,6 +46,7 @@ def register(request):
                 user.save()
                 # print(f"User {username} created!")
                 messages.success(request,f"User {username} created!")
+                return redirect('login')
         else:
             messages.warning(request,'Password not matching...')
             return redirect('register')
